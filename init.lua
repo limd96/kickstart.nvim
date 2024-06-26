5982e2dad43406560cae122d1ac9682951b6e305
Damian Lim <damian.lim@oden.io>
2024-06-26 15:32:04 -0400

    update new tab keymap

--- init.lua
+++ init.lua
@@ -178,7 +178,7 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagn
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Tab keymaps
-vim.api.nvim_set_keymap('n', 'tt', ':tabnew<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'tt', ':tabnew | e#<CR>', { noremap = true, silent = true })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
